1f04b1e5fcbfb2af3d09970acfc7731e73a6c231
Florian Schmidt <flo@devflo.de>
2024-03-30 22:53:20 +0100

    fix start

--- start-both.sh
+++ start-both.sh
@@ -4,4 +4,4 @@
 cd api && DATABASE_HOST=$DATABASE_HOST DATABASE_USER=$DATABASE_USER DATABASE_PASS=$DATABASE_PASS DATABASE_NAME=$DATABASE_NAME DATABASE_PORT=$DATABASE_PORT npm start &
 
 # Gehe zum Frontend-Verzeichnis und starte das Frontend
-cd ../app && npm install && npm run build && npx serve -s build -l 5000
+cd app && npm install && npm run build && npx serve -s build -l 5000
